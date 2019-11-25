b04a98ba73b5d9da83b42a4e3b74f451aa705623
dekan <dekanbro@gmail.com>
2019-11-25 15:58:20 -0700

    remove json store from signin

--- src/views/auth/SignIn.js
+++ src/views/auth/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { Auth, Storage } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 import shortid from 'shortid';
 
 import {
@@ -142,29 +142,30 @@ const SignIn = (props) => {
                 }),
                 'custom:encrypted_ks': JSON.stringify(store),
               });
-              const jsonse = JSON.stringify(
-                {
-                  username: user.username,
-                  deviceId: accountDevices.items[0].device.address,
-                },
-                null,
-                2,
-              );
-              const blob = new Blob([jsonse], {
-                type: 'application/json',
-              });
-              try {
-                // save user meta to S3
-                Storage.put(
-                  `member_${account.address.toUpperCase()}.json`,
-                  blob,
-                  {
-                    contentType: 'text/json',
-                  },
-                );
-              } catch (err) {
-                console.log('storage error', err);
-              }
+              // TODO: replace with api 
+              // const jsonse = JSON.stringify(
+              //   {
+              //     username: user.username,
+              //     deviceId: accountDevices.items[0].device.address,
+              //   },
+              //   null,
+              //   2,
+              // );
+              // const blob = new Blob([jsonse], {
+              //   type: 'application/json',
+              // });
+              // try {
+              //   // save user meta to S3
+              //   Storage.put(
+              //     `member_${account.address.toUpperCase()}.json`,
+              //     blob,
+              //     {
+              //       contentType: 'text/json',
+              //     },
+              //   );
+              // } catch (err) {
+              //   console.log('storage error', err);
+              // }
 
               const attributes = await Auth.currentUserInfo();
               const realuser = {
