bb87fc096ab56038e8da30a9e617aca2e3ce64dc
oovg <office@vengist.com>
2019-11-29 10:38:58 -0800

    launch prep. new landing/404

--- src/components/shared/TopNav.js
+++ src/components/shared/TopNav.js
@@ -4,7 +4,7 @@ import { withRouter, Link } from 'react-router-dom';
 import { CurrentUserContext, DaoContext } from '../../contexts/Store';
 import BcToast from './BcToast';
 
-import Brand from '../../assets/PokéMol__Logo.svg';
+import Brand from '../../assets/PokeMol__Logo.svg';
 import './TopNav.scss';
 import useModal from './useModal';
 import Modal from './Modal';
