36a0ad92e09b29241c329202913da978c296cb9b
Dekan <dekanbro@gmail.com>
2020-01-17 16:33:42 -0700

    redirect to correct proposal route

--- src/components/account/Web3SignIn.js
+++ src/components/account/Web3SignIn.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { DaoServiceContext } from '../../contexts/Store';
 
 export const Web3SignIn = ({ history }) => {
-  return (
-    <button
-      onClick={async () => {
-        localStorage.setItem('loginType', 'web3');
-        history.push('/proposals');
-        window.location.reload();
-      }}
-    >
-      Sign In With Web3
+    const [daoService] = useContext(DaoServiceContext);
+    return (
+        <button
+            onClick={async () => {
+                localStorage.setItem('loginType', 'web3');
+                history.push('/dao/'+daoService.daoAddress.toLowerCase()+'/proposals');
+                window.location.reload();
+            }}
+        >
+            Sign In With Web3
     </button>
-  );
+    );
 };
