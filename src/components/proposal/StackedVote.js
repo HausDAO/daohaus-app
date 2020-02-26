8b04a5696d19bd620937b792476be31855e1ea8a
oovg <office@vengist.com>
2020-02-26 15:20:24 -0800

    fix stuff

--- src/components/proposal/StackedVote.js
+++ src/components/proposal/StackedVote.js
@@ -5,11 +5,13 @@ import { DaoServiceContext, DaoDataContext } from '../../contexts/Store';
 import { primary, tertiary, appDark } from '../../variables.styles';
 
 const StackedVoteDiv = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: 47px;
-  width: calc(100% - 150px);
+  position: ${(props) =>
+    props.page === 'ProposalCard' ? 'relative' : 'absolute'};
+  left: ${(props) => (props.page === 'ProposalCard' ? '0' : '50%')};
+  transform: ${(props) => (props.page === 'none' ? '0' : 'translateX(-50%)')};
+  bottom: ${(props) => (props.page === 'ProposalCard' ? '0px' : '47px')};
+  width: ${(props) =>
+    props.page === 'ProposalCard' ? '100%' : 'calc(100% - 150px)'};
 `;
 
 const FullBarDiv = styled.div`
@@ -39,7 +41,6 @@ const YesLabelSpan = styled.span`
     props.page === 'ProposalCard' ? 'right' : 'center'};
   background-color: transparent;
   font-weight: 900;
-  width: 65px;
   color: ${primary};
 `;
 
@@ -56,7 +57,6 @@ const NoLabelSpan = styled.span`
     props.page === 'ProposalCard' ? 'right' : 'center'};
   background-color: transparent;
   font-weight: 900;
-  width: 65px;
   color: ${tertiary};
 `;
 
