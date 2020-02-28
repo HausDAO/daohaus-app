ba11ece29c9fa6ba4db8cdb8b207f439792a31fa
Dekan <dekanbro@gmail.com>
2020-02-28 15:08:03 -0700

    fix button

--- src/components/proposal/ProposalActions.js
+++ src/components/proposal/ProposalActions.js
@@ -3,100 +3,96 @@ import { withApollo } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
 
 import {
-  CurrentUserContext,
-  CurrentWalletContext,
-  DaoServiceContext,
+    CurrentUserContext,
+    CurrentWalletContext,
+    DaoServiceContext,
 } from '../../contexts/Store';
 import TinyLoader from '../shared/TinyLoader';
 
 const ProposalActions = ({ client, proposal, history }) => {
-  const [currentUser] = useContext(CurrentUserContext);
-  const [loading, setLoading] = useState(false);
-  const [currentWallet] = useContext(CurrentWalletContext);
-  const [daoService] = useContext(DaoServiceContext);
+    const [currentUser] = useContext(CurrentUserContext);
+    const [loading, setLoading] = useState(false);
+    const [currentWallet] = useContext(CurrentWalletContext);
+    const [daoService] = useContext(DaoServiceContext);
 
-  const cancelProposal = async (id) => {
-    setLoading(true);
-    try {
-      await daoService.mcDao.cancelProposal(id);
-    } catch (err) {
-      console.log('user rejected or transaction failed');
-    } finally {
-      setLoading(false);
-      history.push(`/dao/${daoService.daoAddress}/proposals`);
-    }
-  };
+    const cancelProposal = async (id) => {
+        setLoading(true);
+        try {
+            await daoService.mcDao.cancelProposal(id);
+        } catch (err) {
+            console.log('user rejected or transaction failed');
+        } finally {
+            setLoading(false);
+            history.push(`/dao/${daoService.daoAddress}/proposals`);
+        }
+    };
 
-  const sponsorProposal = async (id) => {
-    console.log('sponsor ', id);
-    setLoading(true);
-    try {
-      await daoService.mcDao.sponsorProposal(id);
-    } catch (err) {
-      console.log('user rejected or transaction failed');
-    } finally {
-      setLoading(false);
-      history.push(`/dao/${daoService.daoAddress}/proposals`);
-    }
-  };
+    const sponsorProposal = async (id) => {
+        console.log('sponsor ', id);
+        setLoading(true);
+        try {
+            await daoService.mcDao.sponsorProposal(id);
+        } catch (err) {
+            console.log('user rejected or transaction failed');
+        } finally {
+            setLoading(false);
+            history.push(`/dao/${daoService.daoAddress}/proposals`);
+        }
+    };
 
-  const unlock = async (token) => {
-    console.log('unlock ', token);
-    setLoading(true);
-    const ul = await daoService.token.unlock(token);
-    console.log(ul);
+    const unlock = async (token) => {
+        console.log('unlock ', token);
+        setLoading(true);
+        try {
+            await daoService.token.unlock(token);
+        } catch {
+            console.log('failes');
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    setLoading(false);
-  };
+    console.log('currentWallet', currentWallet);
+    console.log('proposal actions', proposal);
 
-  console.log('currentWallet', currentWallet);
 
-  return (
-    <>
-      <>
-        {!proposal.sponsored &&
-          !proposal.cancelled &&
-          proposal.proposer.toLowerCase() ===
-            currentUser.username.toLowerCase() && (
-            <>
-              {loading ? (
-                <TinyLoader />
-              ) : (
-                <button onClick={() => cancelProposal(proposal.proposalId)}>
-                  Cancel My Proposal
-                </button>
-              )}
-            </>
-          )}
-        {currentWallet.allowance > 0 ? (
-          <>
-            {!proposal.sponsored &&
-              !proposal.cancelled &&
-              currentWallet.shares > 0 && (
-                <>
-                  {loading ? (
+
+    return (
+        <>
+            {loading ? (
+                <button>
                     <TinyLoader />
-                  ) : (
-                    <button
-                      onClick={() => sponsorProposal(proposal.proposalId)}
-                    >
-                      Sponsor Proposal
-                    </button>
-                  )}
-                </>
-              )}
-          </>
-        ) : (
-          <div
-            className="UnlockButton"
-            onClick={() => unlock('0xd0a1e359811322d97991e03f863a0c30c2cf029c')}
-          >
-            {!loading ? <span>! Unlock</span> : <TinyLoader />}
-          </div>
-        )}
-      </>
-    </>
-  );
+                </button>
+            ) : (
+                    <>
+                        {!proposal.sponsored &&
+                            !proposal.cancelled &&
+                            proposal.proposer.toLowerCase() ===
+                            currentUser.username.toLowerCase() && (
+                                <button onClick={() => cancelProposal(proposal.proposalId)}>
+                                    <span>Cancel My Proposal</span>
+                                </button>
+                            )}
+                        {currentWallet.allowance > 0 ? (
+                            <>
+                                {!proposal.sponsored &&
+                                    !proposal.cancelled &&
+                                    currentWallet.shares > 0 && (
+                                        <button
+                                            onClick={() => sponsorProposal(proposal.proposalId)}>
+                                            <span>Sponsor Proposal</span>
+                                        </button>
+                                    )}
+                            </>
+                        ) : (
+                                <button className="UnlockButton" onClick={() => unlock(proposal.moloch.depositToken.tokenAddress)}>
+                                    <span>Unlock Token To Sponsor</span>
+                                </button>
+                            )}
+                    </>
+                )}
+        </>
+    );
 };
 
 export default withRouter(withApollo(ProposalActions));
