fd75014a8f4968c4b46351350621d75b5272f6a9
Dekan <dekanbro@gmail.com>
2020-03-05 15:51:46 -0700

    change proposal id to index on process

--- src/views/proposal/Proposal.js
+++ src/views/proposal/Proposal.js
@@ -51,13 +51,13 @@ const Proposal = (props) => {
     setTxLoading(true);
     try {
       if(proposal.whitelist) {        
-        await daoService.mcDao.processWhitelistProposal(proposal.proposalId);
+        await daoService.mcDao.processWhitelistProposal(proposal.proposalIndex);
 
       } else if (proposal.guildkick) {
-        await daoService.mcDao.processGuildKickProposal(proposal.proposalId);
+        await daoService.mcDao.processGuildKickProposal(proposal.proposalIndex);
 
       } else {
-        await daoService.mcDao.processProposal(proposal.proposalId);
+        await daoService.mcDao.processProposal(proposal.proposalIndex);
       }
     } catch (e) {
       console.error(`Error processing proposal: ${e.toString()}`);
