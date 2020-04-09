fdb23945ce8cbc3b50ca7ad3fe526d895482bd03
Dekan <dekanbro@gmail.com>
2020-04-09 11:37:23 -0600

    anyToBn does not covert decimal values

--- src/utils/Helpers.js
+++ src/utils/Helpers.js
@@ -5,11 +5,21 @@ export const truncateAddr = (addr) => {
 };
 
 export const valToDecimalString = (value, tokenAddress, tokens) => {
+
   const tdata = tokens.find((token) => token.value === tokenAddress);
+  console.log(tdata.decimals, value);
+  if(value>1){
+
   const decimals = anyToBN(tdata.decimals);
-  const exp = anyToBN(10).pow(decimals)
+  const exp = anyToBN(10).pow(decimals);
   // keep big number a string
   console.log('decimalstring', anyToBN(value).mul(exp));
   
   return anyToBN(value).mul(exp);
+  } else {
+  console.log('decimalstring lt 1', value * 10 ** tdata.decimals);
+
+    return value * 10 ** tdata.decimals
+  }
+
 };
