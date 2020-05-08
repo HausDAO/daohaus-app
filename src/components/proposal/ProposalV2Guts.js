75a9d9c337bbc40a784305640a55ebb42ae5aedf
Dekan <dekanbro@gmail.com>
2020-05-08 15:53:18 -0600

    fix links to member page

--- src/components/proposal/ProposalV2Guts.js
+++ src/components/proposal/ProposalV2Guts.js
@@ -5,7 +5,9 @@ import ProposalKickedMember from './ProposalKickedMember';
 import config from '../../config';
 
 const ProposalGutsV2 = ({ proposal, daoData }) => {
-  const memberUrlV2 = `/dao/${daoData.contractAddress}/member/${daoData.contractAddress}-member-${proposal.sponsor}`;
+  const memberUrlV2 = (addr) => {
+    return  `/dao/${daoData.contractAddress}/member/${daoData.contractAddress}-member-${addr}`
+  }
   return (
     <div className="ProposalGuts">
       {proposal.cancelled && <p style={{ color: 'red' }}>Proposal Cancelled</p>}
@@ -14,7 +16,7 @@ const ProposalGutsV2 = ({ proposal, daoData }) => {
         <>
           <h5 className="Label">Sponsored By</h5>
           <p className="Data">
-            <a href={memberUrlV2}>{proposal.sponsor}</a>
+            <a href={memberUrlV2(proposal.sponsor)}>{proposal.sponsor}</a>
           </p>
         </>
       ) : null}
