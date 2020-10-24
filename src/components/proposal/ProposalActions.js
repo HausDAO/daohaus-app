a84a4ad362dfda489c700fd2223ceed64756992b
Scott Herren <me@stwoh.com>
2020-10-24 14:55:31 -0500

    add deposit required to context

--- src/components/proposal/ProposalActions.js
+++ src/components/proposal/ProposalActions.js
@@ -118,7 +118,7 @@ const ProposalActions = ({ client, proposal, history }) => {
                   className="UnlockButton"
                   onClick={() => alert('wrapeth.com')}
                 >
-                  <span>Get some {proposal.moloch.depositToken.symbol}!</span>
+                  <span>Get {proposal.moloch.proposalDeposit / 10 ** 18} {proposal.moloch.depositToken.symbol}!</span>
                 </button>
               )}
             </>
