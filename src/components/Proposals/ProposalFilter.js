7f9d8502064f7d62ff216701b940f567f5524a5d
skuhlmann <samkuhlmann@gmail.com>
2020-11-04 15:37:03 -0700

    raw select

---
+++ src/components/Proposals/ProposalFilter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const options = [
+  {
+    name: 'Action Needed',
+    value: 'actionNeeded',
+  },
+  {
+    name: 'Funding Propsals',
+    value: 'funding',
+  },
+  {
+    name: 'Member Proposal',
+    value: 'member',
+  },
+  {
+    name: 'Whitelist Token Proposal',
+    value: 'whitelist',
+  },
+  {
+    name: 'Trade Proposal',
+    value: 'trade',
+  },
+];
+
+const ProposalFilter = ({ allProposals, setFilteredRecords }) => {
+  return <div>ProposalFilter</div>;
+};
+
+export default ProposalFilter;
