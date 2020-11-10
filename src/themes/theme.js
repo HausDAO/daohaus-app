1c8fb99aad542d5fe2c43f579acde74b441ac337
oovg <office@vengist.com>
2020-11-10 15:40:25 -0800

    starts base input theming

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -158,6 +158,35 @@ export const setTheme = (daoTheme) => {
           size: 'md',
         },
       },
+      Input: {
+        parts: ['field', 'addon'],
+        baseStyle: {
+          field: {
+            borderColor: 'whiteAlpha.50',
+            color: 'whiteAlpha.900',
+            focusBorderColor: 'secondary.500',
+          },
+          addon: {
+            color: 'black',
+            bg: 'transparent',
+            focusBorderColor: 'secondary.500',
+          },
+        },
+        variants: {},
+        sizes: {},
+        defaultProps: {
+          size: 'md',
+          focusBorderColor: 'secondary.500',
+        },
+      },
+      Textarea: {
+        baseStyle: {
+          color: 'white',
+        },
+        defaultProps: {
+          focusBorderColor: 'secondary.500',
+        },
+      },
       Link: {
         baseStyle: {
           _hover: { textDecoration: 'none', color: 'secondary.500' },
