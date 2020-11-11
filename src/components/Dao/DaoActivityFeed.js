cd8cbbabc5005ee33f96ccc66788d7433f4b73a0
skuhlmann <samkuhlmann@gmail.com>
2020-11-11 14:21:06 -0700

    pagination

--- src/components/Dao/DaoActivityFeed.js
+++ src/components/Dao/DaoActivityFeed.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Box } from '@chakra-ui/core';
+import Pagination from 'rc-pagination';
 
 import GraphFetch from '../Shared/GraphFetch';
 import { DAO_ACTIVITIES } from '../../utils/apollo/dao-queries';
@@ -7,23 +8,44 @@ import { activitiesData } from '../../content/skeleton-data';
 import DaoActivityCard from '../Activities/DaoActivityCard';
 import { getDaoActivites } from '../../utils/activities-helpers';
 
+const PER_PAGE = 3;
+
 const DaoActivityFeed = ({ dao }) => {
   const [fetchedData, setFetchedData] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
   const [activities, setActivities] = useState(activitiesData);
+  const [allActivities, setAllActivities] = useState(activitiesData);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     if (fetchedData) {
       console.log('fetchedData', fetchedData);
-      const allActivites = getDaoActivites(fetchedData);
-      console.log('allActivites', allActivites);
+      const hydratedActivites = getDaoActivites(fetchedData);
+      console.log('hydratedActivites', hydratedActivites);
 
-      setActivities(allActivites);
+      setAllActivities(hydratedActivites);
+      setActivities(filterVisibleActivites(hydratedActivites, currentPage));
+
       setIsLoaded(true);
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchedData]);
+
+  const handlePageChange = (page) => {
+    console.log('handlePageClick', page);
+    setCurrentPage(page);
+    setActivities(filterVisibleActivites(allActivities, page));
+  };
+
+  const filterVisibleActivites = (acts, page) => {
+    const start = (page - 1) * PER_PAGE;
+    const end = start + PER_PAGE;
+
+    console.log('start, end ', start, end);
+    return acts.slice(start, end);
+  };
 
   return (
     <>
@@ -44,6 +66,16 @@ const DaoActivityFeed = ({ dao }) => {
           isLoaded={isLoaded}
         />
       ))}
+
+      {isLoaded ? (
+        <Pagination
+          onChange={handlePageChange}
+          current={currentPage}
+          total={allActivities.length}
+          pageSize={PER_PAGE}
+          hideOnSinglePage={true}
+        />
+      ) : null}
 
       {dao ? (
         <GraphFetch
