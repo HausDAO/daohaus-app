1c7edf46b890e2634172ad632ce2b926cdb05134
skuhlmann <samkuhlmann@gmail.com>
2020-11-30 07:58:21 -0700

    finishes withdraw

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Flex } from '@chakra-ui/core';
+
 import ProfileOverviewCard from '../../components/Profile/OverviewCard';
-import { useMembers, useDaoGraphData } from '../../contexts/PokemolContext';
+import { useMembers, useRefetchQuery } from '../../contexts/PokemolContext';
 import ProfileActvityFeed from '../../components/Profile/ProfileActivityFeed';
 import TokenList from '../../components/Shared/TokenList/TokenList';
 
 const Profile = () => {
   const params = useParams();
   const [members] = useMembers();
-  const [dao] = useDaoGraphData();
   const [memberProfile, setMemberProfile] = useState(null);
+  const [, updateRefetchQuery] = useRefetchQuery();
 
   useEffect(() => {
     if (members.length > 0) {
@@ -21,13 +22,21 @@ const Profile = () => {
         ),
       );
     }
-  }, [members, params, dao]);
+  }, [members, params]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateRefetchQuery('proposals');
+    }, 350000);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <Flex>
       <Box w='60%' pl={6}>
         {memberProfile && <ProfileOverviewCard user={memberProfile} />}
-        <TokenList tokenList={memberProfile?.tokenBalances} />
+        <TokenList tokenList={memberProfile?.tokenBalances} isMember={true} />
       </Box>
       <Box pl={6}>
         <ProfileActvityFeed profileAddress={params.id} />
