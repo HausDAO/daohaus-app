6ff2586a70f315b44984fbdb5598b37d210294a3
Scott Herren <me@stwoh.com>
2020-12-03 20:59:42 -0600

    use select for applicant input
    CU-g6wvhq[qa]
    CU-g8u6j8[qa]

--- src/components/Forms/AddressInput.js
+++ src/components/Forms/AddressInput.js
@@ -1,17 +1,32 @@
+import React, { useState } from 'react';
 import {
   FormControl,
   FormHelperText,
   FormLabel,
   Input,
+  Select,
+  Button,
+  Flex,
 } from '@chakra-ui/react';
+import { AiOutlineCaretDown } from 'react-icons/ai';
 import TextBox from '../Shared/TextBox';
-import React from 'react';
-// import { useForm } from 'react-hook-form';
-import { useEns } from '../../contexts/PokemolContext';
+import { useEns, useMembers, useUser } from '../../contexts/PokemolContext';
+import { truncateAddr } from '../../utils/helpers';
 
-const AddressInput = ({ register, setValue, watch, formLabel }) => {
+const AddressInput = ({
+  register,
+  setValue,
+  watch,
+  formLabel,
+  guildKick,
+  newMember,
+  member,
+}) => {
   formLabel = formLabel || 'applicant';
   const [ens] = useEns();
+  const [user] = useUser();
+  const [anyApplicant, setAnyApplicant] = useState(false);
+  const [members] = useMembers();
 
   const ensAddr = watch('applicantHidden', '');
 
@@ -29,28 +44,69 @@ const AddressInput = ({ register, setValue, watch, formLabel }) => {
   };
 
   return (
-    <FormControl mb={5}>
-      <TextBox as={FormLabel} htmlFor='applicant'>
-        {formLabel}
-      </TextBox>
-      <Input
-        name='applicant'
-        placeholder='0x'
-        ref={register({
-          required: {
-            value: true,
-            message: `${formLabel} is required`,
-          },
-        })}
-        color='white'
-        focusBorderColor='secondary.500'
-        onChange={handleChange}
-      />
-      <FormHelperText fontSize='xs' id='applicant-helper-text' mb={1}>
-        {ensAddr || 'Use ETH address or ENS'}
-      </FormHelperText>
-      <Input type='hidden' name='applicantHidden' ref={register} />
-    </FormControl>
+    <>
+      {anyApplicant || member ? (
+        <FormControl mb={5}>
+          <TextBox as={FormLabel} htmlFor='applicant'>
+            {formLabel}
+          </TextBox>
+          <Input
+            name='applicant'
+            placeholder='0x'
+            defaultValue={newMember ? user.username : null}
+            ref={register({
+              required: {
+                value: true,
+                message: `${formLabel} is required`,
+              },
+            })}
+            color='white'
+            focusBorderColor='secondary.500'
+            onChange={handleChange}
+          />
+          <FormHelperText fontSize='xs' id='applicant-helper-text' mb={1}>
+            {ensAddr || 'Use ETH address or ENS'}
+          </FormHelperText>
+          <Input type='hidden' name='applicantHidden' ref={register} />
+        </FormControl>
+      ) : (
+        <FormControl mb={5}>
+          <TextBox as={FormLabel} htmlFor='applicant'>
+            {formLabel}
+          </TextBox>
+          <Flex>
+            <Select
+              icon={<AiOutlineCaretDown />}
+              name='applicant'
+              ref={register}
+            >
+              {members &&
+                members.map((member) => {
+                  return (
+                    <option
+                      key={member.memberAddress}
+                      value={member.memberAddress}
+                    >
+                      {member.profile.name
+                        ? member.profile.name
+                        : truncateAddr(member.memberAddress)}
+                    </option>
+                  );
+                })}
+            </Select>
+            {!guildKick && (
+              <Button
+                onClick={() => setAnyApplicant(true)}
+                ml='2px'
+                variant='outline'
+              >
+                Other
+              </Button>
+            )}
+          </Flex>
+        </FormControl>
+      )}
+    </>
   );
 };
 
