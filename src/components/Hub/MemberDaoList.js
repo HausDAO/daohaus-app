2dcf210e179a144dace6d9cb11c96ad24e11bb3f
skuhlmann <samkuhlmann@gmail.com>
2020-12-30 16:03:01 -0700

    fixes the bad spinner and hopefully cut down on a few renders

--- src/components/Hub/MemberDaoList.js
+++ src/components/Hub/MemberDaoList.js
@@ -65,7 +65,7 @@ const MemberDaoList = ({ daos }) => {
               whiteSpace: 'nowrap',
             }}
           >
-            {dao.title}
+            {dao.apiMetadata[0].name}
           </Box>
         </Link>
       </Box>
--- src/views/Settings/CustomTheme.js
+++ src/views/Settings/CustomTheme.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Flex, Box, Button, Icon, HStack } from '@chakra-ui/react';
-import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+// import { Link as RouterLink, useHistory } from 'react-router-dom';
+
 import { BiArrowBack } from 'react-icons/bi';
 
 import { useTempTheme, useTheme } from '../../contexts/CustomThemeContext';
 import { defaultTheme } from '../../themes/theme-defaults';
-import { useDao } from '../../contexts/PokemolContext';
-import { boostPost } from '../../utils/requests';
+import { useDao, useUser, useWeb3Connect } from '../../contexts/PokemolContext';
+// import { boostPost } from '../../utils/requests';
 import ThemeColorsForm from '../../components/Forms/CustomThemeForm';
 import ThemePreview from '../../components/Settings/ThemePreview';
 
 const CustomTheme = () => {
+  const [web3Connect] = useWeb3Connect();
+  const [user] = useUser();
   const [dao] = useDao();
   const [theme, setTheme] = useTheme();
-  const history = useHistory();
+  // const history = useHistory();
   const [tempTheme, setTempTheme] = useTempTheme();
   const [previewTheme, setPreviewTheme] = useState({
     primary500: theme.colors.primary[500],
@@ -34,14 +38,25 @@ const CustomTheme = () => {
   };
 
   const saveTheme = async () => {
+    const messageHash = web3Connect.web3.utils.sha3(dao.address);
+    const signature = await web3Connect.web3.eth.personal.sign(
+      messageHash,
+      user.username,
+    );
+
+    // console.log('sig', sig);
+
     const updateThemeObject = {
       contractAddress: dao.address,
       boostKey: 'customTheme',
       metadata: tempTheme,
+      signature,
     };
-    const result = await boostPost('dao/boost', updateThemeObject);
-    console.log(result);
-    history.push(`/dao/${dao.address}/settings`);
+    console.log('saving', updateThemeObject);
+
+    // const result = await boostPost('dao/boost', updateThemeObject);
+    // console.log(result);
+    // history.push(`/dao/${dao.address}/settings`);
   };
 
   // SET DEFAULT THEME PREVIEW
