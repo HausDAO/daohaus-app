56e7912bf2e6f694e83fd7d6feab87572d798c50
oovg <office@vengist.com>
2021-01-06 14:17:43 -0800

    refactors layout for presetCard and summonStepOne

--- src/components/Summon/SummonStepOne.js
+++ src/components/Summon/SummonStepOne.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Box, Text, Button, Flex } from '@chakra-ui/react';
+import { Box, Heading, Text, Button, Flex } from '@chakra-ui/react';
 import { RiArrowRightFill } from 'react-icons/ri';
 import { daoPresets } from '../../content/summon-presets';
 import PresetCard from './PresetCard';
 import { useNetwork } from '../../contexts/PokemolContext';
+import TextBox from '../Shared/TextBox';
+
+import {
+  formatPeriodDuration,
+  formatPeriodLength,
+  formatDepositWei,
+} from '../../utils/helpers';
 
 const SummonStepOne = ({ daoData, setDaoData, setCurrentStep }) => {
   const [network] = useNetwork();
@@ -33,15 +40,87 @@ const SummonStepOne = ({ daoData, setDaoData, setCurrentStep }) => {
         <a href='/help#xDAI'>Quick Start Guide</a> on how to switch to xDAI for
         cheaper, faster interactions for your community.
       </Text>
-      <Flex wrap='wrap'>{renderPresets()}</Flex>
-      <Box className='StepControl'>
-        <Button
-          onClick={() => setCurrentStep(2)}
-          disabled={!daoData.presetName}
-          className={!daoData.presetName ? 'disabled' : ''}
+      <Box
+        d='flex'
+        wrap='wrap'
+        p='0px'
+        bg='blackAlpha.800'
+        borderWidth='1px'
+        borderColor='whiteAlpha.200'
+        borderRadius={5}
+      >
+        <Flex direction='column' w={['100%', null, null, null, '30%']}>
+          {renderPresets()}
+        </Flex>
+        <Flex
+          direction='column'
+          w={['100%', null, null, null, '70%']}
+          borderLeftWidth='1px'
+          borderColor='whiteAlpha.200'
         >
-          Continue <RiArrowRightFill />
-        </Button>
+          {daoData?.currency ? (
+            <Box mt={4} p={6}>
+              <Text
+                as='h2'
+                fontFamily='heading'
+                fontSize='2xl'
+                style={{ color: daoData.color }}
+              >
+                {daoData.presetName}
+              </Text>
+              <Text>{daoData.presetDescription}</Text>
+              <TextBox mt={4}>Settings</TextBox>
+              <Text>
+                Currency: <strong>{daoData.currency}</strong>
+              </Text>
+              <Text>
+                Period Duration:{' '}
+                <strong>{formatPeriodDuration(daoData.periodDuration)}</strong>
+              </Text>
+              <Text>
+                Voting Period:{' '}
+                <strong>
+                  {formatPeriodLength(
+                    daoData.votingPeriod,
+                    daoData.periodDuration,
+                  )}
+                </strong>
+              </Text>
+              <Text>
+                Grace Period:{' '}
+                <strong>
+                  {formatPeriodLength(
+                    daoData.gracePeriod,
+                    daoData.periodDuration,
+                  )}
+                </strong>
+              </Text>
+              <Text>
+                Prop Deposit:{' '}
+                <strong>{`${formatDepositWei(daoData.proposalDeposit)} ${
+                  daoData.currency
+                }`}</strong>
+              </Text>
+              <Text>
+                Prop Reward:{' '}
+                <strong>{`${formatDepositWei(daoData.processingReward)} ${
+                  daoData.currency
+                }`}</strong>
+              </Text>
+              <Box className='StepControl'>
+                <Button
+                  onClick={() => setCurrentStep(2)}
+                  disabled={!daoData.presetName}
+                  className={!daoData.presetName ? 'disabled' : ''}
+                >
+                  Continue <RiArrowRightFill />
+                </Button>
+              </Box>
+            </Box>
+          ) : (
+            <Box p={6}>Select a preset</Box>
+          )}
+        </Flex>
       </Box>
     </Box>
   );
