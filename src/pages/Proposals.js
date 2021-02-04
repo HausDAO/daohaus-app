e35da700e934f4a6d5ea1f956a9116c6919a8c51
skuhlmann <samkuhlmann@gmail.com>
2021-02-04 15:32:33 -0700

    gets proposal filters working

--- src/pages/Proposals.js
+++ src/pages/Proposals.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 
 import ActivitiesFeed from '../components/activitiesFeed';
-import ProposalCard from '../components/proposalCard';
 import { getProposalsActivites } from '../utils/activities';
-import Paginator from '../components/paginator';
+import ProposalsList from '../components/proposalList';
 
 const Proposals = React.memo(function Proposals({ proposals, activities }) {
-  const [allProposals, setAllProposals] = useState(null);
-  const [pagedProposals, setPagedProposals] = useState(null);
-
-  useEffect(() => {
-    if (proposals) {
-      setAllProposals(proposals);
-    }
-  }, [proposals]);
-
   return (
     <Flex wrap='wrap'>
       <Box
@@ -23,17 +13,7 @@ const Proposals = React.memo(function Proposals({ proposals, activities }) {
         pr={[0, null, null, null, 6]}
         pb={6}
       >
-        {pagedProposals &&
-          pagedProposals
-            .slice(0, 5)
-            .map((proposal) => (
-              <ProposalCard key={proposal.id} proposal={proposal} />
-            ))}
-        <Paginator
-          perPage={3}
-          setRecords={setPagedProposals}
-          allRecords={allProposals}
-        />
+        <ProposalsList proposals={proposals} />
       </Box>
 
       <Box w={['100%', null, null, null, '40%']}>
