fd09aaaa0934b80a63df600f2deaa6909bfe3b56
skuhlmann <samkuhlmann@gmail.com>
2021-02-05 08:02:46 -0700

    adds member sort and generalizes the sort component

--- src/components/exploreFilters.js
+++ src/components/exploreFilters.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Flex } from '@chakra-ui/react';
 
 import ExploreSearch from './exploreSearch';
-import ExploreSort from './exploreSort';
 import ExploreTagList from './exploreTagList';
 import ExploreFilterList from './exploreFilterList';
+import { ExploreContext } from '../contexts/ExploreContext';
+import ListSort from './listSort';
+import { SORT_OPTIONS } from '../utils/exploreContent';
 
 const ExploreFilters = () => {
+  const [sort, setSort] = useState();
+  const { dispatch } = useContext(ExploreContext);
+
+  useEffect(() => {
+    if (sort) {
+      dispatch({ type: 'updateSort', payload: sort });
+    }
+  }, [sort]);
+
   return (
     <>
       <Flex align='center' justify='flex-start' wrap='wrap'>
         <ExploreSearch />
-        <ExploreSort />
+        <ListSort sort={sort} setSort={setSort} options={SORT_OPTIONS} />
         <ExploreFilterList />
       </Flex>
 
