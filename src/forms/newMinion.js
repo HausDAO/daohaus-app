295019ab0a92aae2615bf08f21a406e2a15f7d71
skuhlmann <samkuhlmann@gmail.com>
2021-02-05 15:11:59 -0700

    preps for tx

--- src/forms/newMinion.js
+++ src/forms/newMinion.js
@@ -6,101 +6,169 @@ import {
   FormControl,
   FormHelperText,
   Heading,
+  Icon,
   Input,
   Link,
+  Spinner,
 } from '@chakra-ui/react';
-import TextBox from '../components/TextBox';
+import { RiExternalLinkLine } from 'react-icons/ri';
 import { useForm } from 'react-hook-form';
-import { MinionFactoryService } from '../services/minionFactoryService';
-import { supportedChains } from '../utils/chain';
 
-import {
-  useDao,
-  // useTxProcessor,
-  // useUser,
-  // useWeb3Connect,
-  // useModals,
-  // useNetwork,
-} from '../contexts/DaoContext';
+import { MinionFactoryService } from '../services/minionFactoryService';
+import { txUri } from '../utils/chain';
 import { useInjectedProvider } from '../contexts/InjectedProviderContext';
+import { createPoll } from '../services/pollService';
+import { useUser } from '../contexts/UserContext';
+import { useOverlay } from '../contexts/OverlayContext';
+import { useDao } from '../contexts/DaoContext';
 
 const NewMinionForm = () => {
   const [loading, setLoading] = useState(false);
   const { daochain, daoid } = useParams();
-  const { address, injectedProvider } = useInjectedProvider();
-  const { daoOverview } = useDao();
-  // const [web3Connect] = useWeb3Connect();
-  // const [network] = useNetwork();
-  // const { closeModals } = useModals();
-  // const [txProcessor, updateTxProcessor] = useTxProcessor();
+  const { address, injectedProvider, injectedChain } = useInjectedProvider();
+  const { daoOverview, refetch } = useDao();
+  const { cachePoll, resolvePoll } = useUser();
+  const { errorToast, successToast } = useOverlay();
   const { handleSubmit, register } = useForm();
+  const [step, setStep] = useState(1);
+  const [pendingTx, setPendingTx] = useState(null);
+  const now = (new Date().getTime() / 1000).toFixed();
 
-  const onSubmit = (values) => {
+  const onSubmit = async (values) => {
     setLoading(true);
+    setStep(2);
 
-    // console.log(values);
-    // const setupValues = {
-    //   minionFactory: supportedChains[network.network_id].minion_factory_addr,
-    //   actionVlaue: '0',
-    // };
-    // const minionFactoryService = new MinionFactoryService(
-    //   web3Connect.web3,
-    //   user.username,
-    //   setupValues,
-    // );
+    // args = [daoAddress, details]
+    const summonParams = [daoid, values.details];
 
-    // try {
-    //   minionFactoryService.summonMinion(
-    //     dao.address,
-    //     values.details,
-    //     txCallBack,
-    //   );
-    // } catch (err) {
-    //   setLoading(false);
-    //   console.log('error: ', err);
-    // }
+    try {
+      const poll = createPoll({ action: 'summonMinion', cachePoll })({
+        chainID: injectedChain.chain_id,
+        molochAddress: daoid,
+        createdAt: now,
+        actions: {
+          onError: (error, txHash) => {
+            console.error(`error: ${error}`);
+            errorToast({
+              title: `There was an error.`,
+            });
+            resolvePoll(txHash);
+            setStep(1);
+          },
+          onSuccess: (txHash) => {
+            const title = values.details
+              ? `${values.details} Lives!`
+              : 'Minion Lives!';
+            successToast({
+              title,
+            });
+            refetch();
+            resolvePoll(txHash);
+            setStep('success');
+          },
+        },
+      });
+
+      const onTxHash = (txHash) => {
+        console.log('tx', txHash);
+        setPendingTx(txHash);
+      };
+
+      await MinionFactoryService({
+        web3: injectedProvider,
+        chainID: injectedChain.chain_id,
+      })('summonMinion')({ args: summonParams, from: address, poll, onTxHash });
+    } catch (err) {
+      console.log('error in tx', err);
+      setLoading(false);
+      setStep(1);
+      errorToast({
+        title: `There was an error.`,
+        description: err?.message || '',
+      });
+    }
   };
-
-  console.log('daoOverview', daoOverview);
 
   return (
     <Box w='90%'>
-      <Heading as='h4' size='md' fontWeight='100' mb={10}>
-        Deploy Your Minion
-      </Heading>
-      {daoOverview?.minions.length > 0 && (
+      {step === 1 ? (
         <>
-          <Box mb={5} fontSize='md'>
-            You have {daoOverview.minions.length} minion
-            {daoOverview.minions.length > 1 ? 's' : ''} already. Are you looking
-            for the{' '}
-            <Link
-              as={RouterLink}
-              to={`/dao/${daochain}/${daoid}/settings`}
-              color='secondary.500'
-            >
-              Settings?
-            </Link>
+          <Heading as='h4' size='md' fontWeight='100' mb={10}>
+            Deploy Your Minion
+          </Heading>
+          {daoOverview?.minions.length > 0 && (
+            <>
+              <Box mb={5} fontSize='md'>
+                You have {daoOverview.minions.length} minion
+                {daoOverview.minions.length > 1 ? 's' : ''} already. Are you
+                looking for the{' '}
+                <Link
+                  as={RouterLink}
+                  to={`/dao/${daochain}/${daoid}/settings`}
+                  color='secondary.500'
+                >
+                  Settings?
+                </Link>
+              </Box>
+            </>
+          )}
+          <Box mb={3} fontSize='sm'>
+            Deploying a Minion will allow the DAO to interact with external
+            contracts through proposals
+          </Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box mb={3} fontSize='sm'>
+              <FormControl mb={5}>
+                <FormHelperText fontSize='sm' id='name-helper-text' mb={3}>
+                  A Minion needs a name
+                </FormHelperText>
+                <Input
+                  name='details'
+                  placeholder='Frank'
+                  w='60%'
+                  ref={register}
+                />
+              </FormControl>
+            </Box>
+            <Button type='submit' isLoading={loading}>
+              Deploy
+            </Button>
+          </form>
+        </>
+      ) : null}
+
+      {step === 2 ? (
+        <>
+          <Heading as='h4' size='md' fontWeight='100' mb={10}>
+            Deploying Your Minion
+          </Heading>
+          <Spinner />
+
+          <Box my={10}>
+            {pendingTx ? (
+              <Link
+                href={`${txUri(daochain)}/${pendingTx}`}
+                isExternal
+                fontSize='2xl'
+                color='secondary.500'
+              >
+                View Transaction <Icon as={RiExternalLinkLine} />
+              </Link>
+            ) : null}
           </Box>
         </>
-      )}
-      <Box mb={3} fontSize='sm'>
-        Deploying a Minion will allow the DAO to make arbitrary transaction
-        calls from successful proposals.
-      </Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Box mb={3} fontSize='sm'>
-          <FormControl mb={5}>
-            <Input name='details' placeholder='Frank' w='60%' ref={register} />
-            <FormHelperText fontSize='sm' id='name-helper-text' mb={1}>
-              Give your Minion a name.
-            </FormHelperText>
-          </FormControl>
-        </Box>
-        <Button type='submit' isLoading={loading}>
-          Deploy
-        </Button>
-      </form>
+      ) : null}
+
+      {step === 'success' ? (
+        <>
+          <Heading as='h4' size='md' fontWeight='100' mb={10}>
+            A Minion is ready at your service
+          </Heading>
+          <Button as={RouterLink} to={`/dao/${daochain}/${daoid}/settings`}>
+            Settings
+          </Button>
+        </>
+      ) : null}
     </Box>
   );
 };
--- src/services/minionFactoryService.js
+++ src/services/minionFactoryService.js
@@ -3,79 +3,35 @@ import Web3 from 'web3';
 import MinionFactoryAbi from '../contracts/minionFactory.json';
 import { chainByID } from '../utils/chain';
 
-export const MinionFactoryService = ({
-  web3,
-  daoAddress,
-  version,
-  chainID,
-}) => {
-  // console.log('web3', web3);
-  // console.log('daoAddress', daoAddress);
-  // console.log('version', version);
-  // console.log('chainID', chainID);
+export const MinionFactoryService = ({ web3, chainID }) => {
   if (!web3) {
     const rpcUrl = chainByID(chainID).rpc_url;
     web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl));
   }
   const abi = MinionFactoryAbi;
-  const setupValues = {
-    minionFactory: chainByID(chainID).minion_factory_addr,
-    actionValue: '0',
-  };
-  const contract = new web3.eth.Contract(abi, setupValues);
-
-  return function getService(service) {
-    // console.log('service', service);
-
-    if (service === 'getUserTokenBalance') {
-      return async ({ userAddress, tokenAddress }) => {
-        const balance = await contract.methods
-          .getUserTokenBalance(userAddress, tokenAddress)
-          .call();
-        return balance;
-      };
-    }
-    if (service === 'members') {
-      return async (memberAddress) => {
-        const member = await contract.methods.members(memberAddress).call();
-        return member;
-      };
-    }
-    if (service === 'memberAddressByDelegateKey') {
-      return async (memberAddress) => {
-        const address = await contract.methods
-          .memberAddressByDelegateKey(memberAddress)
-          .call();
-        return address.toLowerCase();
-      };
-    }
-    if (
-      service === 'submitProposal' ||
-      service === 'sponsorProposal' ||
-      service === 'cancelProposal' ||
-      service === 'submitVote' ||
-      service === 'processProposal' ||
-      service === 'processWhitelistProposal' ||
-      service === 'processGuildKickProposal' ||
-      service === 'submitWhitelistProposal' ||
-      service === 'submitGuildKickProposal'
-    ) {
-      return async ({ args, address, poll, onTxHash }) => {
-        // console.log('args', args);
-        // console.log('from', address);
-        // console.log('poll', poll);
-        const tx = await contract.methods[service](...args);
-        return tx
-          .send('eth_requestAccounts', { from: address })
-          .on('transactionHash', (txHash) => {
-            if (poll) {
-              onTxHash();
-              poll(txHash);
-            }
-          })
-          .on('error', (error) => {
-            console.error(error);
-          });
+  const contract = new web3.eth.Contract(
+    abi,
+    chainByID(chainID).moloch_factory_addr,
+  );
+  return (service) => {
+    if (service === 'summonMinion') {
+      return async ({ args, from, poll, onTxHash }) => {
+        try {
+          const tx = await contract.methods[service](...args);
+          return tx
+            .send({ from })
+            .on('transactionHash', (txHash) => {
+              if (poll) {
+                onTxHash(txHash);
+                poll(txHash);
+              }
+            })
+            .on('error', (error) => {
+              console.error(error);
+            });
+        } catch (error) {
+          return error;
+        }
       };
     }
   };
