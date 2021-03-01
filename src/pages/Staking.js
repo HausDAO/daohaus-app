c9e1b07fc816de9ddf59ad3f6b62cea6bfe35bbf
jordanlesich <jordan.lesich@gmail.com>
2021-02-28 21:58:07 -0500

    roughed in staking page ui

--- src/pages/Staking.js
+++ src/pages/Staking.js
@@ -1,136 +1,20 @@
 import React from 'react';
-import {
-  Box,
-  Flex,
-  Button,
-  Text,
-  InputGroup,
-  FormLabel,
-  Input,
-  InputRightAddon,
-  FormHelperText,
-} from '@chakra-ui/react';
-import { Link as RouterLink, useParams } from 'react-router-dom';
+import { Box, Flex, Button, useBreakpointValue, Icon } from '@chakra-ui/react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import ContentBox from '../components/ContentBox';
 import TextBox from '../components/TextBox';
-// import { useMetaData } from '../contexts/MetaDataContext';
-import { boostList } from '../content/boost-content';
-import GenericModal from '../modals/genericModal';
+import StakingSnapshot from '../components/stakingSnapshot';
+import ClanCard from '../components/clanCard';
+import RonanCard from '../components/ronanCard';
+import ShogunCard from '../components/shogunCard';
+
 import { useOverlay } from '../contexts/OverlayContext';
-import BoostLaunchWrapper from '../components/boostLaunchWrapper';
 import MainViewLayout from '../components/mainViewLayout';
-import { useInjectedProvider } from '../contexts/InjectedProviderContext';
-import { daoConnectedAndSameChain } from '../utils/general';
-import { getTerm } from '../utils/metadata';
+import { FaCopy } from 'react-icons/fa';
 
-const Staking = ({ customTerms, daoMember, daoOverview, daoMetaData }) => {
-  // const { daoMetaData } = useMetaData();
-
-  // const canInteract =
-  //   daoConnectedAndSameChain(address, injectedChain?.chainId, daochain) &&
-  //   +daoMember?.shares > 0;
-
-  // const hasDependentBoost = (boostKey) => {
-  //   if (boostKey === 'vanillaMinions') {
-  //     const minions = daoOverview.minions.length;
-  //     return minions;
-  //   }
-  //   const boostData = daoMetaData.boosts[boostKey];
-  //   console.log(daoMetaData.boosts, daoOverview);
-  //   return boostData && boostData.active;
-  // };
-
-  // const renderBoostCard = (boost, i) => {
-  //   const boostData = daoMetaData.boosts && daoMetaData.boosts[boost.key];
-  //   const hasBoost = boostData && boostData.active;
-
-  //   return (
-  //     <ContentBox
-  //       d='flex'
-  //       key={i}
-  //       w={['100%', '100%', '40%', '30%']}
-  //       h='370px'
-  //       m={3}
-  //       mb={6}
-  //       p={6}
-  //       flexDirection='column'
-  //       alignItems='center'
-  //       justifyContent='space-around'
-  //     >
-  //       <Box fontFamily='heading' fontSize='2xl' fontWeight={700}>
-  //         {boost.name}
-  //       </Box>
-  //       <Box textAlign='center'>{boost.description}</Box>
-  //       {boost.price === '0' ? (
-  //         <Box textAlign='center' fontFamily='heading'>
-  //           Cost
-  //         </Box>
-  //       ) : null}
-
-  //       <Box textAlign='center'>
-  //         {boost.price === '0' ? (
-  //           <Box fontFamily='heading' fontSize='xl' m={0}>
-  //             Free
-  //           </Box>
-  //         ) : null}
-  //       </Box>
-  //       {boost.comingSoon ? (
-  //         <Button textTransform='uppercase' disabled={true}>
-  //           Coming Soon
-  //         </Button>
-  //       ) : (
-  //         <>
-  //           {hasBoost ? (
-  //             <Button
-  //               as={RouterLink}
-  //               to={`/dao/${daochain}/${daoid}/settings/${boost.successRoute}`}
-  //               textTransform='uppercase'
-  //               disabled={!canInteract}
-  //             >
-  //               Settings
-  //             </Button>
-  //           ) : (
-  //             <>
-  //               {boost.dependency && !hasDependentBoost(boost?.dependency) ? (
-  //                 <Button textTransform='uppercase' disabled={true}>
-  //                   Needs {boost.dependency}
-  //                 </Button>
-  //               ) : (
-  //                 <Button
-  //                   textTransform='uppercase'
-  //                   onClick={() => setGenericModal({ [boost.key]: true })}
-  //                   disabled={!canInteract}
-  //                 >
-  //                   Add This App
-  //                 </Button>
-  //               )}
-  //             </>
-
-  //             // <Button
-  //             //   textTransform='uppercase'
-  //             //   onClick={() => setGenericModal({ [boost.key]: true })}
-  //             //   disabled={!canInteract}
-  //             // >
-  //             //   Add This {getTerm(customTerms, 'boost')}
-  //             // </Button>
-  //           )}
-  //         </>
-  //       )}
-  //       <GenericModal closeOnOverlayClick={false} modalId={boost.key}>
-  //         <>
-  //           {!boost.comingSoon ? (
-  //             <>
-  //               <BoostLaunchWrapper boost={boost} />
-  //             </>
-  //           ) : null}
-  //         </>
-  //       </GenericModal>
-  //     </ContentBox>
-  //   );
-  // };
+const Staking = () => {
   const onStakeHaus = () => {};
-  // const
 
   const StakeHausBtn = () => (
     <Button textTransform='uppercase' onClick={onStakeHaus}>
@@ -144,130 +28,102 @@ const Staking = ({ customTerms, daoMember, daoOverview, daoMetaData }) => {
         <TextBox size='sm' mb={3} textTransform='uppercase'>
           stake $haus, earn $haus
         </TextBox>
-
-        <Flex>
-          <StakeCard
-            title='Ronan'
-            description='Stake $Haus on your own'
-            reward='Get 1x Rewards, Soft Signaling'
-            label='My Wallet: 314.51489 $HAUS'
-          />
-          <StakeCard title='Clan' />
-          <StakeCard title='Shogun' mr='auto' />
-          {/* {daoMetaData
-            ? boostList.map((boost, i) => {
-              return renderBoostCard(boost, i);
-            })
-          : null} */}
+        <Flex
+          align='start'
+          justify='space-between'
+          flexDir={['column', null, null, null, 'row']}
+        >
+          <Flex
+            minWidth={[null, null, null, '888px']}
+            width='100%'
+            maxWidth='1090px'
+            justifyContent={['space-around', null, null, 'flex-start']}
+            mb={4}
+            wrap={['wrap', null, null, 'nowrap']}
+          >
+            <RonanCard />
+            <ClanCard />
+            <ShogunCard />
+          </Flex>
           <PriceBox />
         </Flex>
         <TextBox size='sm' mb={3} textTransform='uppercase'>
           staking snapshot
         </TextBox>
       </Box>
+      <StakingSnapshot />
     </MainViewLayout>
   );
 };
 
 export default Staking;
 
-const StakeCard = (props) => {
-  const { title, description, reward, label, ...rest } = props;
+const PriceBox = ({ tokenData }) => {
+  const { successToast } = useOverlay();
+  const addr = '0x1...';
 
-  const handleClick = () => {};
-  return (
-    <ContentBox
-      d='flex'
-      minWidth='260px'
-      width='100%'
-      maxWidth='350px'
-      mr={4}
-      {...rest}
-      flexDirection='column'
-      alignItems='center'
-      justifyContent='space-around'
-    >
-      <Box fontFamily='heading' fontSize='2xl' mb={3} fontWeight={700}>
-        {title}
-      </Box>
-      <Box fontSize='sm' textTransform='center'>
-        {description}
-      </Box>
-      <Box fontSize='sm' textAlign='center' mb={3}>
-        {reward}
-      </Box>
-      <Flex
-        maxWidth='250px'
-        width='100%'
-        minWidth='200px'
-        justifyContent='flex-start'
-      >
-        <Box as={FormLabel} fontSize='xs' color='whiteAlpha.700'>
-          {label}
-        </Box>
-      </Flex>
-      <InputGroup
-        maxWidth='250px'
-        width='100%'
-        minWidth='200px'
-        justifyContent='center'
-      >
-        <Input
-          // name='tributeOffered'
-          placeholder='0'
-          mb={5}
-          // ref={register({
-          color='white'
-          focusBorderColor='secondary.500'
-          // onChange={handleChange}
-        />
-        <InputRightAddon background='primary.500' p={0}>
-          <Button size='md' variant='text'>
-            Max
-          </Button>
-        </InputRightAddon>
-      </InputGroup>
-      <Button
-        px='2.5rem'
-        mb={5}
-        // size='lg'
-        textTransform='uppercase'
-        onClick={handleClick}
-      >
-        Stake
-      </Button>
-      <Flex align='center' mb={3}>
-        <TextBox size='xs' transform='translateY(2px)' mr={2}>
-          Earned:{' '}
+  const handleCopy = () =>
+    successToast({
+      title: 'Copied Address',
+    });
+
+  const WideBox = () => (
+    <ContentBox d='flex' width='100%' justifyContent='space-around' mb={6}>
+      <Flex alignItems='center' width='100%'>
+        <TextBox size='xs' mr={3} transform='translateY(2px)'>
+          Symbol:
         </TextBox>
-        <Box fontFamily='mono' fontSize='xl' fontWeight={700}>
-          {title}
-        </Box>
+        <Flex fontFamily='mono' fontSize='lg' fontWeight={700}>
+          HAUS
+          <CopyToClipboard text={addr} onCopy={handleCopy} ml={4}>
+            <Icon as={FaCopy} _hover={{ cursor: 'pointer' }} />
+          </CopyToClipboard>
+        </Flex>
       </Flex>
-      <Flex>
-        <Button size='sm' variant='outline' px='1.5rem' mr={4}>
-          Harvest
-        </Button>
-        <Button size='sm' variant='outline' px='1.5rem'>
-          Withdraw All
-        </Button>
+      <Flex alignItems='center'>
+        <TextBox size='xs' mr={2} transform='translateY(2px)'>
+          Price:
+        </TextBox>
+        <Box fontFamily='mono' fontSize='lg' fontWeight={700}>
+          $17.81
+        </Box>
       </Flex>
     </ContentBox>
   );
-};
 
-const PriceBox = () => (
-  <ContentBox
-    d='flex'
-    // key={i}
-    width='350px'
-    mr={4}
-    // h='370px'
-    // m={3}
-    // mb={6}
-    // p={6}
-    flexDirection='column'
-    alignItems='center'
-    justifyContent='space-around'
-  ></ContentBox>
-);
+  const ShortBox = () => (
+    <ContentBox
+      d='flex'
+      width='100%'
+      maxWidth='350px'
+      minWidth='250px'
+      ml={4}
+      justifyContent='space-between'
+      height='150px'
+    >
+      <Flex flexDirection='column'>
+        <TextBox size='xs' mb='4'>
+          Symbol:
+        </TextBox>
+        <Flex fontFamily='mono' fontSize='lg' fontWeight={700}>
+          HAUS
+          <CopyToClipboard text={addr} onCopy={handleCopy} ml={4}>
+            <Icon as={FaCopy} _hover={{ cursor: 'pointer' }} />
+          </CopyToClipboard>
+        </Flex>
+      </Flex>
+      <Flex flexDirection='column' alignItems='flex-end'>
+        <TextBox size='xs' mb='4'>
+          Price:
+        </TextBox>
+        <Box fontFamily='mono' fontSize='lg' fontWeight={700}>
+          $17.81
+        </Box>
+      </Flex>
+    </ContentBox>
+  );
+
+  const variant = useBreakpointValue({ xl: <ShortBox />, base: <WideBox /> });
+
+  return <>{variant} </>;
+};
