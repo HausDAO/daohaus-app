f99d53da0ea3410d9097087860d1ab7c3cfd1ad7
skuhlmann <samkuhlmann@gmail.com>
2021-03-18 09:28:01 -0600

    adds banner

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TopNav from '../shared/TopNav';
 import styled from 'styled-components';
+import { DaoDataContext } from '../../contexts/Store';
+
+export const StyledBanner = styled.header`
+  background-color: rgb(16, 21, 61);
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1em;
+  width: calc(100% - 30px);
+  padding: 0px 15px;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.05);
+  h1 {
+    color: white;
+  }
+  a {
+    font-size: 20px;
+  }
+`;
 
 export const StyledHeader = styled.header`
   background-color: ${(props) => props.theme.appBackground};
@@ -16,10 +35,23 @@ export const StyledHeader = styled.header`
 `;
 
 const Header = () => {
+  const [daoData] = useContext(DaoDataContext);
   return (
-    <StyledHeader>
-      <TopNav />
-    </StyledHeader>
+    <>
+      {daoData && daoData.version !== 1 ? (
+        <StyledBanner>
+          <h1>There is a new version of the Pokemol DAO interface!</h1>
+          <p className="DaoText">
+            <a href="https://app.daohaus.club/">
+              Use it now at https://app.daohaus.club/
+            </a>
+          </p>
+        </StyledBanner>
+      ) : null}
+      <StyledHeader>
+        <TopNav />
+      </StyledHeader>
+    </>
   );
 };
 export default Header;
