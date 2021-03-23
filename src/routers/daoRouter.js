a89d9d1996c95d63d2559cb6663f481031c01f27
skuhlmann <samkuhlmann@gmail.com>
2021-03-23 08:54:07 -0600

    adds better withdraw loot values to cco

--- src/routers/daoRouter.js
+++ src/routers/daoRouter.js
@@ -24,7 +24,6 @@ import DiscourseSettings from '../pages/DiscourseSettings';
 import ProposalTypes from '../pages/ProposalTypes';
 import MinionSafe from '../pages/MinionSafe';
 import CcoContribution from '../pages/CcoContribution';
-// Needed when cco is active
 import CcoHelper from '../pages/CcoHelper';
 import Staking from '../pages/Staking';
 import Clone from '../pages/Clone';
@@ -230,12 +229,12 @@ const DaoRouter = () => {
             daoProposals={daoProposals}
           />
         </Route>
-        {/* <Route exact path={`${path}/cco/spy`}>
+        <Route exact path={`${path}/cco/spy`}>
           <CcoHelper
             daoMetaData={daoMetaData}
             currentDaoTokens={currentDaoTokens}
             daoProposals={daoProposals}
-          /> */}
+          />
         </Route>
       </Switch>
     </Layout>
