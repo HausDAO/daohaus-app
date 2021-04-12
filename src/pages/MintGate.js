339642cf36c3789e278fab6a03cdddb709a4cd2b
Scott Herren <me@stwoh.com>
2021-04-12 15:13:46 -0500

    catch fetch errors

--- src/pages/MintGate.js
+++ src/pages/MintGate.js
@@ -20,13 +20,15 @@ const MintGate = ({ daoMetaData }) => {
       fetch(
         // `${MINTGATE_URL}/links?tid=${daoid}`,
         `${MINTGATE_URL}/links?tid=${'0xef3d8c4fbb1860fceab16595db7e650cd5ad51c1'}`,
-      ).then(async (response) => {
-        const result = await response.json();
-        console.log(result);
-        if (result.links?.length > 0) {
-          setGates(result.links);
-        }
-      });
+      )
+        .then(async (response) => {
+          const result = await response.json();
+          console.log(result);
+          if (result.links?.length > 0) {
+            setGates(result.links);
+          }
+        })
+        .catch((err) => console.log(err));
     };
     // if (daoid && daoMetaData && 'mintGate' in daoMetaData?.boosts) {
     fetchGates();
