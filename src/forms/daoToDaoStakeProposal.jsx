69656df73ba79b3743797948f7906dd17d6c24ee
skuhlmann <samkuhlmann@gmail.com>
2021-04-16 10:32:09 -0600

    adds a note to uber staking form

--- src/forms/daoToDaoStakeProposal.jsx
+++ src/forms/daoToDaoStakeProposal.jsx
@@ -214,27 +214,40 @@ const StakeProposalForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl
-        isInvalid={errors.name}
-        display='flex'
-        flexDirection='row'
-        justifyContent='space-between'
-        mb={5}
-        flexWrap='wrap'
-      >
-        <Box w={['100%', null, '50%']} pr={[0, null, 5]}>
-          <DetailsFields register={register} />
+    <>
+      <Box mb={4}>
+        <Box variant='heading' fontWeight={500}>
+          Membership Requirements
         </Box>
-        <Box w={['100%', null, '50%']}>
-          <TextBox as={FormLabel} size='xs' htmlFor='name' mb={2}>
-            Shares Requested
-          </TextBox>
-          <Input
-            name='sharesRequested'
-            placeholder='0'
-            mb={5}
-            ref={register({
+        <Box>
+          Minimum tribute: 500 HAUS
+        </Box>
+        <Box>
+          Shares requested: Equal to tribute
+        </Box>
+      </Box>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl
+          isInvalid={errors.name}
+          display='flex'
+          flexDirection='row'
+          justifyContent='space-between'
+          mb={5}
+          flexWrap='wrap'
+        >
+          <Box w={['100%', null, '50%']} pr={[0, null, 5]}>
+            <DetailsFields register={register} />
+          </Box>
+          <Box w={['100%', null, '50%']}>
+            <TextBox as={FormLabel} size='xs' htmlFor='name' mb={2}>
+              Shares Requested
+            </TextBox>
+            <Input
+              name='sharesRequested'
+              placeholder='0'
+              mb={5}
+              ref={register({
               required: {
                 value: true,
                 message: 'Requested shares are required for Member Proposals',
@@ -244,58 +257,59 @@ const StakeProposalForm = () => {
                 message: 'Requested shares must be a number',
               },
             })}
-            color='white'
-            focusBorderColor='secondary.500'
-          />
-          <DaoToDaoStakingTributeInput
-            register={register}
-            setValue={setValue}
-            getValues={getValues}
-            setError={setCurrentError}
-            stakingToken={stakingToken}
-          />
-        </Box>
-      </FormControl>
-      <Flex justify='flex-end' align='center' h='60px'>
-        {currentError && (
+              color='white'
+              focusBorderColor='secondary.500'
+            />
+            <DaoToDaoStakingTributeInput
+              register={register}
+              setValue={setValue}
+              getValues={getValues}
+              setError={setCurrentError}
+              stakingToken={stakingToken}
+            />
+          </Box>
+        </FormControl>
+        <Flex justify='flex-end' align='center' h='60px'>
+          {currentError && (
           <Box color='secondary.300' fontSize='m' mr={5}>
             <Icon as={RiErrorWarningLine} color='secondary.300' mr={2} />
             {currentError.message}
           </Box>
         )}
 
-        {noBalance ? (
-          <Box color='secondary.300' fontSize='m' mr={5}>
-            <Icon as={RiErrorWarningLine} color='secondary.300' mr={2} />
-            {`You can&apos;t stake into UberHAUS until your UberHAUS minion has
+          {noBalance ? (
+            <Box color='secondary.300' fontSize='m' mr={5}>
+              <Icon as={RiErrorWarningLine} color='secondary.300' mr={2} />
+              {`You can&apos;t stake into UberHAUS until your UberHAUS minion has
             a ${UBERHAUS_DATA.STAKING_TOKEN_SYMBOL} balance. Send 
             ${UBERHAUS_DATA.STAKING_TOKEN_SYMBOL} your minion&apos;s address:
             ${stakingToken?.uberHausMinionData.minionAddress}`}
-            <CopyToClipboard
-              text={stakingToken?.uberHausMinionData.minionAddress}
-              onCopy={copiedToast}
-            >
-              <Icon
-                as={FaCopy}
-                color='secondary.300'
-                ml={2}
-                _hover={{ cursor: 'pointer' }}
-              />
-            </CopyToClipboard>
-          </Box>
+              <CopyToClipboard
+                text={stakingToken?.uberHausMinionData.minionAddress}
+                onCopy={copiedToast}
+              >
+                <Icon
+                  as={FaCopy}
+                  color='secondary.300'
+                  ml={2}
+                  _hover={{ cursor: 'pointer' }}
+                />
+              </CopyToClipboard>
+            </Box>
         ) : null}
-        <Box>
-          <Button
-            type='submit'
-            loadingText='Submitting'
-            isLoading={loading}
-            disabled={loading || noBalance}
-          >
-            Submit
-          </Button>
-        </Box>
-      </Flex>
-    </form>
+          <Box>
+            <Button
+              type='submit'
+              loadingText='Submitting'
+              isLoading={loading}
+              disabled={loading || noBalance}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Flex>
+      </form>
+    </>
   );
 };
 
