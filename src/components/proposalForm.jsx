be255e650a500274a4eb94dbbd2d25fb9b586feb
Jordan Lesich <jordan.lesich@gmail.com>
2021-05-03 11:11:55 -0400

    additional options

--- src/components/proposalForm.jsx
+++ src/components/proposalForm.jsx
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Box, Flex, FormControl, Input, FormLabel, Textarea, InputLeftAddon, InputGroup,
+  Button,
+  Box,
+  Flex,
+  FormControl,
+  Input,
+  FormLabel,
+  Textarea,
+  Menu,
+  InputLeftAddon,
+  InputGroup,
+  InputRightAddon,
+  Select,
+  MenuButton,
+  Icon,
+  MenuList,
+  MenuItem,
 } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 
+import { RiAddFill } from 'react-icons/ri';
 import TextBox from './TextBox';
 
+// const getHeight = (fields) => {
+//   if (!fields.length > 0) {
+//     throw new Error('Proposal form has zero fields, please input data into the component correctly');
+//   }
+//   if (fields.length > 8) {
+//     return '28rem';
+//   }
+//   if (fields.length > 6) {
+//     return '24rem';
+//   }
+//   if (fields.legth > 4) {
+//     return '22rem';
+//   }
+//   if (fields.length > 0) {
+//     return '18rem';
+//   }
+// };
+
 const ProposalForm = ({
-  fields, tx, onSubmit, onTx,
+  fields, tx, onSubmit, onTx, additionalOptions,
 }) => {
+  const [loading, setLoading] = useState(false);
+  // const { newDaoProposal } = useTX();
   const localForm = useForm;
 
-  console.log(fields);
   return (
     <form>
-      <FormControl
+      <Flex flexDir='column'>
+        <FormControl
         // isInvalid={errors.name}
-        display='flex'
-        mb={5}
-        height='22rem'
+          display='flex'
+          mb={5}
 
-      >
-        <Flex w='100%' flexDirection='column' flexWrap='wrap'>
-          {fields?.map((field) => {
-            return <InputFactory key={field?.htmlFor || field?.name} {...field} localForm={localForm} />;
-          })}
-        </Flex>
-      </FormControl>
+        >
+          <Flex w='100%' flexWrap='wrap' justifyContent='space-between'>
+            {fields?.map((field) => {
+              return <InputFactory key={field?.htmlFor || field?.name} {...field} localForm={localForm} />;
+            })}
+          </Flex>
+        </FormControl>
+        <FormFooter additionalOptions={additionalOptions} />
+      </Flex>
     </form>
   );
 };
@@ -44,33 +81,62 @@ const InputFactory = (props) => {
   if (type === 'linkInput') {
     return <LinkInput {...props} />;
   }
+  if (type === 'inputSelect') {
+    return <InputSelect {...props} />;
+  }
   return null;
 };
 
 const GenericInput = ({
-  label, htmlFor, placeholder, name, valOnType = [], valOnSubmit = [], localForm,
+  label, htmlFor, placeholder, name, valOnType = [], valOnSubmit = [], localForm, append, prepend,
 }) => {
   console.log(label);
   const { register } = localForm();
 
   return (
     <FieldWrapper>
-      <TextBox as={FormLabel} size='xs' htmlFor={htmlFor} mb={2}>
+      <TextBox as={FormLabel} size='xs' htmlFor={htmlFor}>
         {label}
       </TextBox>
-      <Input
-        id={htmlFor}
-        name={name}
-        placeholder={placeholder || label || htmlFor}
-        mb={2}
-        ref={register({
-          required: {
-            value: true,
-            message: 'Required',
-          },
-        })}
-      />
+      <InputGroup>
+        {prepend && <InputLeftAddon background='primary.600'>{prepend}</InputLeftAddon>}
+        <Input
+          id={htmlFor}
+          name={name}
+          placeholder={placeholder || label || htmlFor}
+          mb={2}
+          ref={register({
+            required: {
+              value: true,
+              message: 'Required',
+            },
+          })}
+        />
+        {append && <InputRightAddon background='primary.600' p={0}>{append}</InputRightAddon>}
+      </InputGroup>
     </FieldWrapper>
+  );
+};
+
+const InputSelect = (props) => {
+  const { options } = props;
+
+  return (
+    <GenericInput
+      {...props}
+      append={
+        <Select>
+          {options?.map((option, index) => (
+            <option
+              key={`${option?.value}-${index}`}
+              value={option.value}
+            >
+              {option.name}
+            </option>))}
+        </Select>
+        }
+    />
+
   );
 };
 
@@ -81,7 +147,7 @@ const GenericTextarea = ({
 
   return (
     <FieldWrapper>
-      <TextBox as={FormLabel} size='xs' htmlFor={htmlFor} mb={2}>
+      <TextBox as={FormLabel} size='xs' htmlFor={htmlFor}>
         {label}
       </TextBox>
       <Textarea
@@ -100,26 +166,71 @@ const GenericTextarea = ({
     </FieldWrapper>);
 };
 
-const LinkInput = ({
-  label, htmlFor, placeholder, name, valOnType = [], valOnSubmit = [], localForm, h = 10,
-}) => {
+const LinkInput = (props) => {
   return (
-    <FieldWrapper>
-      <TextBox as={FormLabel} size='xs' htmlFor={htmlFor} mb={2}>
-        {label}
-      </TextBox>
-      <InputGroup>
-        <InputLeftAddon background='primary.600'>https://</InputLeftAddon>
-        <Input
-          id={htmlFor}
-          name={name}
-          placeholder={placeholder || label || htmlFor}
-          mb={2}
-        />
-      </InputGroup>
-    </FieldWrapper>
+    <GenericInput {...props} prepend='https://' />
   );
 };
 const FieldWrapper = ({ children }) => {
-  return <Box w={['100%', null, '50%']} mb={5}>{children}</Box>;
+  return <Box w={['100%', null, '48%']} mb={2}>{children}</Box>;
+};
+
+const AdditionalOptions = ({ options }) => {
+  return (
+    <Box>
+      <Menu color='white' textTransform='uppercase'>
+        <MenuButton
+          as={Button}
+          variant='outline'
+          rightIcon={<Icon as={RiAddFill} />}
+          borderTopRightRadius='0'
+          borderBottomRightRadius='0'
+          type='button'
+        >
+          Additional Options
+        </MenuButton>
+        <MenuList>
+          <MenuItem>
+            Applicant
+          </MenuItem>
+          <MenuItem>
+            Request Loot
+          </MenuItem>
+          <MenuItem>
+            Request Payment
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
+
+const FormFooter = ({ additionalOptions, loading }) => {
+  if (additionalOptions?.length) {
+    return (
+      <Flex justifyContent='flex-end'>
+        <AdditionalOptions mr='auto' />
+        <Button
+          // type='submit'
+          loadingText='Submitting'
+          isLoading={loading}
+          disabled={loading}
+          borderBottomLeftRadius='0'
+          borderTopLeftRadius='0'
+        >
+          Submit
+        </Button>
+      </Flex>
+    );
+  }
+  return (
+    <Button
+      type='submit'
+      loadingText='Submitting'
+      isLoading={loading}
+      disabled={loading}
+    >
+      Submit
+    </Button>
+  );
 };
--- src/staticElements/proposalFormData.js
+++ src/staticElements/proposalFormData.js
@@ -1,5 +1,36 @@
 import { PROPOSAL_TYPES } from '../utils/proposalUtils';
 
+export const FIELD_TYPES = {
+  TITLE: {
+    type: 'input',
+    label: 'Title',
+    name: 'title',
+    htmlFor: 'title',
+    placeholder: 'Proposal Title',
+  },
+  DESCRIPTION: {
+    type: 'textarea',
+    label: 'Description',
+    name: 'description',
+    htmlFor: 'description',
+    placeholder: 'How does that make you feel, champ',
+  },
+  SHARES_REQUEST: {
+    type: 'input',
+    label: 'shares requested',
+    name: 'shares',
+    htmlFor: 'shares',
+    placeholder: '0',
+  },
+  LINK: {
+    type: 'linkInput',
+    label: 'Link',
+    name: 'link',
+    htmlFor: 'link',
+    placeholder: 'daolink.club',
+  },
+};
+
 export const PROPOSAL_FORMS = {
   MEMBER: {
     type: PROPOSAL_TYPES.MEMBER,
@@ -9,27 +40,26 @@ export const PROPOSAL_FORMS = {
       action: 'submitProposal',
     },
     fields: [
+      FIELD_TYPES.TITLE,
+      FIELD_TYPES.SHARES_REQUEST,
+      FIELD_TYPES.DESCRIPTION,
       {
-        type: 'input',
-        label: 'Title',
-        name: 'title',
-        htmlFor: 'title',
-        placeholder: 'Proposal Title',
+        type: 'inputSelect',
+        selectLabel: 'Tests',
+        label: 'Sample Input Select',
+        name: 'generic-input-select',
+        htmlFor: 'generic-input-select',
+        options: [
+          { name: 'Test 1', value: 'test 1' },
+          { name: 'Test 2', value: 'test 2' },
+          { name: 'Test 3', value: 'test 3' },
+        ],
       },
-      {
-        type: 'textarea',
-        label: 'Description',
-        name: 'description',
-        htmlFor: 'description',
-        placeholder: 'How does that make you feel, champ',
-      },
-      {
-        type: 'linkInput',
-        label: 'Link',
-        name: 'link',
-        htmlFor: 'link',
-        placeholder: 'daolink.club',
-      },
+      FIELD_TYPES.LINK,
+    ],
+    additionalOptions: [
+      FIELD_TYPES.TITLE,
+      FIELD_TYPES.SHARES_REQUEST,
     ],
   },
 };
