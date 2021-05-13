2ea333cc70d24786617657b8195a07a88997c98e
oovg <office@vengist.com>
2021-05-13 09:22:51 -0700

    mod p on daosquareheader for center align of header

--- src/components/daoSquareHeader.jsx
+++ src/components/daoSquareHeader.jsx
@@ -17,7 +17,7 @@ const DaosquareHeader = () => {
   }, [address]);
 
   return (
-    <Flex direction='row' justify='space-between' w='100%' p={6}>
+    <Flex direction='row' justify='space-between' w='100%' px={6} py={2}>
       <Link
         as={RouterLink}
         color='mode.900'
