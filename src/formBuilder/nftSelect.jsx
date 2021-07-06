154d66c60d5f4649b250ed60d15b12fc6098643a
Owen Murovec <owenmurovec@gmail.com>
2021-07-06 16:27:12 -0700

    Allows user to select an NFT in a form

--- src/formBuilder/nftSelect.jsx
+++ src/formBuilder/nftSelect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Text, Button, Icon, Select } from '@chakra-ui/react';
+import deepEqual from 'deep-eql';
+import { Box, Text, Flex, Button, Icon, Select, Image } from '@chakra-ui/react';
 import { RiAddFill } from 'react-icons/ri';
 import { useDao } from '../contexts/DaoContext';
 import { useOverlay } from '../contexts/OverlayContext';
@@ -8,25 +9,46 @@ import GenericModal from '../modals/genericModal';
 const NftSelect = ({ label }) => {
   const { setGenericModal } = useOverlay();
   const { daoVaults } = useDao();
+  const [nftData, setNftData] = useState();
   const [nfts, setNfts] = useState();
+  const [selected, setSelected] = useState();
   const [collections, setCollections] = useState();
+  const [filter, setFilter] = useState();
 
   useEffect(() => {
     if (nfts) {
-      nfts.reduce((acc, item) => {
-        if (acc.indexOf(item.collection) !== -1) {
-          return [...acc, item.collection];
-        }
-        return acc;
-      }, []);
+      setCollections(
+        nfts.reduce((acc, item) => {
+          if (acc.indexOf(item.name) === -1) {
+            return [...acc, item.name];
+          }
+          return acc;
+        }, []),
+      );
     }
   }, [nfts]);
 
   useEffect(() => {
     if (daoVaults) {
-      setNfts(daoVaults.reduce((acc, item) => [...acc, ...item.nfts], []));
+      const data = daoVaults.reduce((acc, item) => [...acc, ...item.nfts], []);
+      setNftData(data);
+      setNfts(data);
     }
   }, [daoVaults]);
+
+  useEffect(() => {
+    if (nftData && filter) {
+      const filtered = nftData.filter(item => {
+        if (filter !== '') {
+          return filter === item.name;
+        }
+        return true;
+      });
+      if (!deepEqual(filtered, nfts)) {
+        setNfts(filtered);
+      }
+    }
+  }, [filter, nfts]);
 
   const selectModal = (
     <GenericModal closeOnOverlayClick modalId='nftSelect'>
@@ -41,7 +63,34 @@ const NftSelect = ({ label }) => {
         >
           Select An NFT
         </Box>
-        <Select placeholder='Filter by Collection' />
+        <Select
+          placeholder='Filter by Collection'
+          onChange={e => setFilter(e.nativeEvent.target.value)}
+        >
+          {collections?.map(collection => (
+            <option value={collection} key={collection}>
+              {collection}
+            </option>
+          ))}
+        </Select>
+        {nfts?.map((nft, i) => (
+          <Box key={i} mt={5}>
+            <Flex mb={5} alignItems='center' justify='space-between'>
+              <Text textTransform='uppercase' fontFamily='header'>
+                {nft.metadata.name}
+              </Text>
+              <Button
+                onClick={() => {
+                  setGenericModal({});
+                  setSelected(nft);
+                }}
+              >
+                Select
+              </Button>
+            </Flex>
+            <Image src={nft.metadata.image} />
+          </Box>
+        ))}
       </Box>
     </GenericModal>
   );
@@ -50,14 +99,29 @@ const NftSelect = ({ label }) => {
     <>
       <Box>
         <Text mb={3}>{label}</Text>
-        <Button
-          variant='nftSelect'
-          onClick={() => {
-            setGenericModal({ nftSelect: true });
-          }}
-        >
-          <Icon w={50} h={50} color='primary.500' as={RiAddFill} />
-        </Button>
+        {selected ? (
+          <Image
+            onClick={() => {
+              setGenericModal({ nftSelect: true });
+            }}
+            _hover={{
+              opacity: 0.5,
+              cursor: 'pointer',
+            }}
+            src={selected.metadata.image}
+            w='300px'
+            h='300px'
+          />
+        ) : (
+          <Button
+            variant='nftSelect'
+            onClick={() => {
+              setGenericModal({ nftSelect: true });
+            }}
+          >
+            <Icon w={50} h={50} color='primary.500' as={RiAddFill} />
+          </Button>
+        )}
       </Box>
       {selectModal}
     </>
