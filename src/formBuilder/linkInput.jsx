20974a06ab6fce643e2335e20ab073c9ea5dc19f
jordan <jordan.lesich@gmail.com>
2021-08-31 08:50:30 -0700

    clean up link input after example

--- src/formBuilder/linkInput.jsx
+++ src/formBuilder/linkInput.jsx
@@ -1,27 +1,6 @@
-import React, { useEffect } from 'react';
-import { validate } from '../utils/validation';
+import React from 'react';
 
 import GenericInput from './genericInput';
 
-const FORM_DISABLE = {
-  message: 'Nope. Fix your crap, cowboy.',
-};
-
-const LinkInput = props => {
-  const { localForm, name, useFormError } = props;
-  const { removeError, addError } = useFormError();
-  const value = localForm?.watch?.(name);
-
-  useEffect(() => {
-    if (validate.address(value)) {
-      removeError(name, value);
-    } else if (value === '') {
-      removeError(name, value);
-    } else {
-      addError(name, FORM_DISABLE, value);
-    }
-  }, [value, name]);
-
-  return <GenericInput {...props} prepend='https://' />;
-};
+const LinkInput = props => <GenericInput {...props} prepend='https://' />;
 export default LinkInput;
