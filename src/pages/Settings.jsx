af1174ef8d87d3ab44e781c41adbd01fc6a54bab
skuhlmann <samkuhlmann@gmail.com>
2021-09-07 14:11:25 -0600

    gets custom theme working outside boost

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Flex, Link, Stack } from '@chakra-ui/react';
-import { Link as RouterLink, useParams } from 'react-router-dom';
+import { Box, Flex } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
 
-// import BoostStatus from '../components/boostStatus';
-// import Superpowers from '../components/daoSuperpowers';
 import DaoContractSettings from '../components/daoContractSettings';
 import DaoMetaOverview from '../components/daoMetaOverview';
 import TextBox from '../components/TextBox';
-// import Minions from '../components/minionList';
 import MainViewLayout from '../components/mainViewLayout';
-import { useInjectedProvider } from '../contexts/InjectedProviderContext';
-import { daoConnectedAndSameChain } from '../utils/general';
 import { fetchTransmutation, getWrapNZap } from '../utils/theGraph';
 
 const Settings = ({ overview, daoMember, daoMetaData, customTerms }) => {
   const { daochain, daoid } = useParams();
-  const { address, injectedChain } = useInjectedProvider();
   const [wrapNZap, setWrapNZap] = useState(null);
   const [transmutationContract, setTransmutationContract] = useState(null);
 
@@ -32,7 +26,6 @@ const Settings = ({ overview, daoMember, daoMetaData, customTerms }) => {
           transmutationRes.transmutations[0].transmutation,
         );
       }
-      // setTransmutationContract();
     };
     getWNZ();
   }, [daoid]);
@@ -54,45 +47,9 @@ const Settings = ({ overview, daoMember, daoMetaData, customTerms }) => {
           />
           <Flex justify='space-between' mt={6}>
             <TextBox size='xs'>DAO Metadata</TextBox>
-            {daoConnectedAndSameChain(
-              address,
-              daochain,
-              injectedChain?.chainId,
-            ) && +daoMember?.shares > 0 ? (
-              <Link
-                as={RouterLink}
-                color='secondary.500'
-                fontFamily='heading'
-                fontSize='xs'
-                textTransform='uppercase'
-                letterSpacing='0.15em'
-                to={`/dao/${daochain}/${daoid}/settings/meta`}
-              >
-                Edit
-              </Link>
-            ) : null}
           </Flex>
-          <DaoMetaOverview daoMetaData={daoMetaData} />
+          <DaoMetaOverview daoMetaData={daoMetaData} daoMember={daoMember} />
         </Box>
-        <Stack w={['100%', null, null, null, '50%']} spacing={4}>
-          {/* {daoMetaData?.boosts &&
-          Object.keys(daoMetaData?.boosts).length > 0 ? (
-            <Stack spacing={2}>
-              <TextBox size='xs'>Superpowers</TextBox>
-              <Superpowers daoMetaData={daoMetaData} daoMember={daoMember} />
-            </Stack>
-          ) : null}
-          {overview?.minions?.length > 0 && (
-            <Stack spacing={2}>
-              <TextBox size='xs'>Minions</TextBox>
-              <Minions />
-            </Stack>
-          )} */}
-          {/* <Stack spacing={2}>
-            <TextBox size='xs'>Boost Status</TextBox>
-            <BoostStatus />
-          </Stack> */}
-        </Stack>
       </Flex>
     </MainViewLayout>
   );
