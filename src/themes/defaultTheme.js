9da4645cd655e3a5dec538f7164dfd89dd887ec4
skuhlmann <samkuhlmann@gmail.com>
2021-10-25 11:29:28 -0600

    default theme update

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -1,22 +1,52 @@
 import { rgba } from 'polished';
 
 import BrandImg from '../assets/img/Daohaus__Castle--Dark.svg';
-import BgImg from '../assets/img/daohaus__hero--falling.jpg';
+// import BgImg from '../assets/img/daohaus__hero--falling.jpg';
+
+// export const defaultTheme = {
+//   primary500: '#10153d',
+//   primaryAlpha: rgba('#10153d', 0.9),
+//   secondary500: '#EB8A23',
+//   secondaryAlpha: rgba('#10153d', 0.75),
+//   bg500: '#03061B',
+//   bgAlpha: '#03061B',
+//   bgOverlayOpacity: 0.75,
+//   modeAlpha500: '#FFFFFF',
+//   headingFont: 'Mulish',
+//   bodyFont: 'Rubik',
+//   monoFont: 'Space Mono',
+//   avatarImg: BrandImg,
+//   bgImg: BgImg,
+//   daoMeta: {
+//     proposals: 'Proposals',
+//     proposal: 'Proposal',
+//     bank: 'Bank',
+//     members: 'Members',
+//     member: 'Member',
+//     boosts: 'Apps',
+//     boost: 'App',
+//     settings: 'Settings',
+//     ragequit: 'Rage Quit',
+//     guildKick: 'Guild Kick',
+//     minion: 'Minion',
+//     minions: 'Minions',
+//   },
+// };
 
 export const defaultTheme = {
-  primary500: '#10153d',
-  primaryAlpha: rgba('#10153d', 0.9),
-  secondary500: '#EB8A23',
-  secondaryAlpha: rgba('#10153d', 0.75),
-  bg500: '#03061B',
+  primary500: '#4a5443',
+  primaryAlpha: rgba(74, 84, 67, 0.9),
+  secondary500: '#d29100',
+  secondaryAlpha: rgba(210, 145, 0, 0.75),
+  bg500: '#000000',
   bgAlpha: '#03061B',
-  bgOverlayOpacity: 0.75,
+  bgOverlayOpacity: 0.41,
   modeAlpha500: '#FFFFFF',
   headingFont: 'Mulish',
   bodyFont: 'Rubik',
   monoFont: 'Space Mono',
   avatarImg: BrandImg,
-  bgImg: BgImg,
+  bgImg: 'QmU34AoT8VedXx1DV2FRsSKXmchEqgEYPs7dsdsRre4WUz',
   daoMeta: {
     proposals: 'Proposals',
     proposal: 'Proposal',
@@ -30,9 +60,5 @@ export const defaultTheme = {
     guildKick: 'Guild Kick',
     minion: 'Minion',
     minions: 'Minions',
-    // f04title: "404 What's Lost Can Be Found",
-    // f04heading: 'You have been slain',
-    // f04subhead: 'Please reload from the most recent save point.',
-    // f04cta: 'Start Over',
   },
 };
