cff4a50b436a438240c13423d1ae3b8b3832d975
jordan <jordan.lesich@gmail.com>
2021-10-29 14:36:11 -0700

    alt

--- src/graphQL/dao-queries.js
+++ src/graphQL/dao-queries.js
@@ -145,6 +145,201 @@ export const DAO_ACTIVITIES = gql`
   }
 `;
 
+export const ALT_ACTIVITIES = gql`
+  query molochActivities($contractAddr: String!, $skip: Int) {
+    moloch(id: $contractAddr) {
+      id
+      version
+      proposals(where: { sponsored: true }, first: 100, skip: $skip) {
+        id
+        aborted
+        applicant
+        cancelled
+        cancelledAt
+        createdAt
+        createdBy
+        details
+        didPass
+        executed
+        gracePeriodEnds
+        guildkick
+        isMinion
+        lootRequested
+        memberAddress
+        newMember
+        noShares
+        noVotes
+        paymentRequested
+        paymentTokenDecimals
+        paymentTokenSymbol
+        processed
+        processor
+        processedAt
+        proposer
+        proposalId
+        proposalIndex
+        sharesRequested
+        sponsored
+        sponsor
+        sponsoredAt
+        startingPeriod
+        trade
+        tributeOffered
+        tributeTokenDecimals
+        tributeTokenSymbol
+        tributeToken
+        votingPeriodStarts
+        votingPeriodEnds
+        whitelist
+        yesShares
+        yesVotes
+        molochAddress
+        molochVersion
+        minionAddress
+        uberHausMinionExecuted
+        minion {
+          minionType
+          minQuorum
+        }
+        actions {
+          target
+          data
+          memberOnly
+        }
+        moloch {
+          gracePeriodLength
+          periodDuration
+          version
+          votingPeriodLength
+        }
+        votes {
+          id
+          memberAddress
+          memberPower
+          uintVote
+          createdAt
+          molochAddress
+        }
+        escrow {
+          tokenAddresses
+          tokenTypes
+          tokenIds
+          amounts
+        }
+      }
+      rageQuits {
+        id
+        createdAt
+        memberAddress
+        shares
+        loot
+      }
+    }
+  }
+`;
+
+export const ALT_AGAIN = gql`
+  query molochActivities($contractAddr: String!, $skip: Int) {
+    moloch(id: $contractAddr) {
+      id
+      version
+      proposals(
+        where: {
+          sponsored: false
+          sharesRequested_gt: 0
+          tributeOffered_gte: "1000000000000000000"
+          tributeToken: "0xb0c5f3100a4d9d9532a4cfd68c55f1ae8da987eb"
+        }
+        first: 100
+        skip: $skip
+      ) {
+        id
+        aborted
+        applicant
+        cancelled
+        cancelledAt
+        createdAt
+        createdBy
+        details
+        didPass
+        executed
+        gracePeriodEnds
+        guildkick
+        isMinion
+        lootRequested
+        memberAddress
+        newMember
+        noShares
+        noVotes
+        paymentRequested
+        paymentTokenDecimals
+        paymentTokenSymbol
+        processed
+        processor
+        processedAt
+        proposer
+        proposalId
+        proposalIndex
+        sharesRequested
+        sponsored
+        sponsor
+        sponsoredAt
+        startingPeriod
+        trade
+        tributeOffered
+        tributeTokenDecimals
+        tributeTokenSymbol
+        tributeToken
+        votingPeriodStarts
+        votingPeriodEnds
+        whitelist
+        yesShares
+        yesVotes
+        molochAddress
+        molochVersion
+        minionAddress
+        uberHausMinionExecuted
+        minion {
+          minionType
+          minQuorum
+        }
+        actions {
+          target
+          data
+          memberOnly
+        }
+        moloch {
+          gracePeriodLength
+          periodDuration
+          version
+          votingPeriodLength
+        }
+        votes {
+          id
+          memberAddress
+          memberPower
+          uintVote
+          createdAt
+          molochAddress
+        }
+        escrow {
+          tokenAddresses
+          tokenTypes
+          tokenIds
+          amounts
+        }
+      }
+      rageQuits {
+        id
+        createdAt
+        memberAddress
+        shares
+        loot
+      }
+    }
+  }
+`;
+
 export const DAO_POLL = gql`
   query moloches($summoner: String!, $createdAt: String!) {
     moloches(where: { summoner: $summoner, createdAt_gt: $createdAt }) {
