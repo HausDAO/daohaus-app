284cb7e505f869eafeaca2af95c34a11343d8bfb
jordan <jordan.lesich@gmail.com>
2022-01-17 23:32:04 -0800

    convert data display into accoridion

--- src/proposalBuilder/multiTxTransfer.jsx
+++ src/proposalBuilder/multiTxTransfer.jsx
@@ -6,8 +6,10 @@ import {
   Button,
   Divider,
   Flex,
+  Icon,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
 import { Link, useParams } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import TextBox from '../components/TextBox';
@@ -72,26 +74,55 @@ const ActionItem = ({ label, data }) => (
   </Box>
 );
 
-const SingleActionDisplay = ({ action, index }) => (
-  <Box mb={6}>
-    <ActionItem label={`TX ${index + 1}`} data={action.data.name} />
-    <ActionItem label='Target Contract' data={action.to} />
-    {action.value !== CONTRACT_ZERO && (
-      <ActionItem label='Target Contract' data={action.to} />
-    )}
-    <TextBox size='xs' variant='label' mb={3}>
-      Parameters:
-    </TextBox>
-    <Box ml='6'>
-      {action.data.params.map((param, index, params) => (
-        <Box key={uuid()} mb={3}>
-          <ActionItem label='Name:' data={param.name} />
-          <ActionItem label='Data Type:' data={param.type} />
-          <ActionItem label='Value:' data={param.value.toString()} />
-          {index !== params?.length - 1 && <Divider />}
-        </Box>
-      ))}
+const SingleActionDisplay = ({ action, index }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(prevState => !prevState);
+  return (
+    <Box mb={6}>
+      <Button
+        variant='ghost'
+        textAlign='left'
+        justifyContent='flex-start'
+        width='100%'
+        px={0}
+        onClick={toggleOpen}
+      >
+        <Flex width='100%' justifyContent='center' alignItems='flex-start'>
+          <ActionItem label={`TX ${index + 1}`} data={action.data.name} />
+          <Icon
+            as={isOpen ? BiChevronDown : BiChevronUp}
+            ml='auto'
+            w='25px'
+            h='25px'
+            cursor='pointer'
+            color='secondary.400'
+            onClick={toggleOpen}
+          />
+        </Flex>
+      </Button>
+      <Divider mb={3} />
+      {isOpen && (
+        <>
+          <ActionItem label='Target Contract' data={action.to} />
+          {action.value !== CONTRACT_ZERO && (
+            <ActionItem label='Target Contract' data={action.to} />
+          )}
+          <TextBox size='xs' variant='label' mb={3}>
+            Parameters:
+          </TextBox>
+          <Box ml='6'>
+            {action.data.params.map((param, index, params) => (
+              <Box key={uuid()} mb={3}>
+                <ActionItem label='Name:' data={param.name} />
+                <ActionItem label='Data Type:' data={param.type} />
+                <ActionItem label='Value:' data={param.value.toString()} />
+                {index !== params?.length - 1 && <Divider />}
+              </Box>
+            ))}
+          </Box>
+          <Divider />
+        </>
+      )}
     </Box>
-    <Divider />
-  </Box>
-);
+  );
+};
