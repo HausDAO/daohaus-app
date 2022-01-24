25ea22482b136013fd8c79b30c5fa60d2edf07ce
skuhlmann <samkuhlmann@gmail.com>
2022-01-24 12:23:38 -0700

    clean up;

--- src/contexts/TokenContext.jsx
+++ src/contexts/TokenContext.jsx
@@ -33,8 +33,6 @@ export const TokenProvider = ({ children }) => {
         setTokenPrices,
       );
 
-      console.log('newDaoData', newDaoData);
-
       setCurrentDaoTokens(newDaoData);
       shouldFetchInit.current = false;
     };
