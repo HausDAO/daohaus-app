8ee768e6e4a32ea6336fa06971a5ae0494ae4690
skuhlmann <samkuhlmann@gmail.com>
2022-03-04 10:38:04 -0700

    ci issues

--- src/formBuilder/switchNetwork.jsx
+++ src/formBuilder/switchNetwork.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Box, Button, Flex } from '@chakra-ui/react';
 
 import { useInjectedProvider } from '../contexts/InjectedProviderContext';
