33be23143cbba273fe6e95b540ce25de00eb9ee5
Santiago Gonzalez <hernangt12re3@gmail.com>
2022-03-31 19:44:28 -0500

    update haus links

--- src/pages/Haus.jsx
+++ src/pages/Haus.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Avatar, Box, Divider, Flex, Text, Tooltip } from '@chakra-ui/react';
+import { Avatar, Box, Divider, Flex, Link, Text } from '@chakra-ui/react';
 import ContentBox from '../components/ContentBox';
 import Layout from '../components/layout';
 import MainViewLayout from '../components/mainViewLayout';
@@ -65,47 +65,35 @@ const Haus = () => {
                   <Text fontSize='xl' mb='13px'>
                     Get Haus
                   </Text>
-                  <Tooltip
-                    label='Coming soon'
-                    araia-label='Coming soon'
-                    placement='right'
-                    hasArrow
+                  <Link
+                    href='https://app.balancer.fi/#/trade/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/0xf2051511B9b121394FA75B8F7d4E7424337af687'
+                    isExternal
                   >
-                    <a
-                      to='https://daohaus.app'
-                      css={{
-                        pointerEvents: 'none',
-                      }}
-                    >
-                      <Text fontSize='sm'>ON ETHEREUM &gt;</Text>
-                    </a>
-                  </Tooltip>
-                  <a href='https://swapr.eth.link/#/swap?chainId=100'>
+                    <Text fontSize='sm'>ON ETHEREUM &gt;</Text>
+                  </Link>
+                  <Link
+                    href='https://swapr.eth.link/#/swap?chainId=100&outputCurrency=0xb0c5f3100a4d9d9532a4cfd68c55f1ae8da987eb'
+                    isExternal
+                  >
                     <Text fontSize='sm'>ON GNOSIS CHAIN &gt;</Text>
-                  </a>
+                  </Link>
                 </Flex>
                 <Flex direction='column'>
                   <Text fontSize='xl' mb='13px'>
                     Provide Liquidity
                   </Text>
-                  <Tooltip
-                    label='Coming soon'
-                    araia-label='Coming soon'
-                    placement='right'
-                    hasArrow
+                  <Link
+                    href='https://app.balancer.fi/#/pool/0x17ddd9646a69c9445cd8a9f921d4cd93bf50d108000200000000000000000159'
+                    isExternal
                   >
-                    <a
-                      to='https://daohaus.app'
-                      css={{
-                        pointerEvents: 'none',
-                      }}
-                    >
-                      <Text fontSize='sm'>ON ETHEREUM &gt;</Text>
-                    </a>
-                  </Tooltip>
-                  <a href='https://swapr.eth.link/#/pools/0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1/0xb0C5f3100A4d9d9532a4CfD68c55F1AE8da987Eb?chainId=100'>
+                    <Text fontSize='sm'>ON ETHEREUM &gt;</Text>
+                  </Link>
+                  <Link
+                    href='https://swapr.eth.link/#/pools/0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1/0xb0C5f3100A4d9d9532a4CfD68c55F1AE8da987Eb?chainId=100'
+                    isExternal
+                  >
                     <Text fontSize='sm'>ON GNOSIS CHAIN &gt;</Text>
-                  </a>
+                  </Link>
                 </Flex>
                 <Flex direction='column'>
                   <Text fontSize='xl' mb='13px'>
