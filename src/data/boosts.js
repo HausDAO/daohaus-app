c0515c0c781f949b1678e3bc437674164e5335c9
Keating <keating.dev@protonmail.com>
2022-05-16 15:29:31 -0400

    initial boost

--- src/data/boosts.js
+++ src/data/boosts.js
@@ -282,6 +282,19 @@ export const CONTENT = {
       { href: 'https://github.com/onPoster', label: 'Poster Github' },
     ],
   },
+  GOOGLE_LIT: {
+    title: 'Lit Google',
+    description: 'Share google documents with the dao',
+    publisher: PUBLISHERS.DAOHAUS,
+    version: '0.1',
+    pars: [
+      'This boost allows daos to share google docs with other users in the dao',
+    ],
+    externalLinks: [
+      { href: 'https://discord.gg/daohaus', label: 'Boost Support' },
+      { href: 'https://github.com/LIT-Protocol', label: 'Lit Github' },
+    ],
+  },
 };
 
 export const COMMON_STEPS = {
@@ -645,6 +658,15 @@ export const BOOSTS = {
       type: 'internalLink',
       appendToDaoPath: 'docs',
     },
+  },
+  GOOGLE_LIT: {
+    id: 'GOOGLE_LIT',
+    boostContent: CONTENT.GOOGLE_LIT,
+    steps: STEPS.BASIC_BOOST,
+    // playlist: BOOST_PLAYLISTS.GOOGLE_LIT,
+    categories: ['community'],
+    networks: { '0x64': true, '0x1': true, '0x4': true }, // TODO: Add more
+    cost: 'free',
   },
 };
 
