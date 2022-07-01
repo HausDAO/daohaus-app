e25ce8f14cf05f12cfc7e7f1dc467a6baed72bea
Scott Herren <me@stwoh.com>
2022-07-01 13:56:34 -0500

    include react in scope

--- src/formBuilder/contributorRewardListInput.jsx
+++ src/formBuilder/contributorRewardListInput.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BigNumber, utils } from 'ethers';
 
 import GenericTextArea from './genericTextArea';
